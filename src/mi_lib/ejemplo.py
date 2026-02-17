5f24ef75bdb52ab00d6a2acc31c1251cf57a973b
luciagonromo <luciagonromo@gmail.com>
2026-02-17 10:46:23 +0100

    Update ejemplo.py

--- src/mi_lib/ejemplo.py
+++ src/mi_lib/ejemplo.py
@@ -1,2 +1,21 @@
 def saluda (nombre):
     print ('¡Hola {}!'.format (nombre))
+
+
+def tablero(dim, muros):
+    filas, columnas = dim
+
+    tab = [[' ' for numero in range(columnas)] for numero in range(filas)]
+
+
+    for f, c in muros:
+        tab[f][c] = 'X'
+
+
+    tab[0][0] = 'E'
+    tab[filas - 1][columnas - 1] = 'S'
+
+    for fila in tab:
+        print(fila)
+
+    return tab
