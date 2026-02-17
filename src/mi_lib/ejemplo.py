45b894b53c2f239bbe35a736459751f0e1c937e3
luciagonromo <luciagonromo@gmail.com>
2026-02-17 11:17:17 +0100

    Update ejemplo.py

--- src/mi_lib/ejemplo.py
+++ src/mi_lib/ejemplo.py
@@ -6,12 +6,8 @@ def tablero(dim, muros):
 
     tab = [[' ' for numero in range(columnas)] for numero in range(filas)]
 
-    # Validar que todos los muros están dentro de mi tablero
-
     for f, c in muros:
         tab[f][c] = 'X'
-
-    # Plantear E y S como argumentos de la función
 
     tab[0][0] = 'E'
     tab[filas - 1][columnas - 1] = 'S'
